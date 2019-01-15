7e0d921366c1ae2643a8bb8b60244aad28f77cc8
v.v.bazhenov@yandex.ru <WebDelphi>
2019-01-16 01:48:56 +0600

    Пример установки своего контрола (кнопки) на ленте Microsoft Excel в Delphi

---
+++ Excel Control Bar/Unit1.dfm
@@ -0,0 +1,52 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  Caption = 'Form1'
+  ClientHeight = 293
+  ClientWidth = 429
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Button1: TButton
+    Left = 8
+    Top = 8
+    Width = 125
+    Height = 25
+    Caption = #1047#1072#1087#1091#1089#1090#1080#1090#1100' Excel'
+    TabOrder = 0
+    OnClick = Button1Click
+  end
+  object Button2: TButton
+    Left = 8
+    Top = 39
+    Width = 125
+    Height = 25
+    Caption = #1042#1089#1077' '#1087#1072#1085#1077#1083#1080
+    TabOrder = 1
+    OnClick = Button2Click
+  end
+  object ListBox1: TListBox
+    Left = 139
+    Top = 0
+    Width = 290
+    Height = 293
+    Align = alRight
+    ItemHeight = 13
+    TabOrder = 2
+  end
+  object Button3: TButton
+    Left = 8
+    Top = 70
+    Width = 125
+    Height = 25
+    Caption = 'Ribbon'
+    TabOrder = 3
+    OnClick = Button3Click
+  end
+end
---
+++ Excel Control Bar/Unit1.pas
@@ -0,0 +1,118 @@
+﻿unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs,ComObj, ActiveX, StdCtrls;
+
+const ExcelApp = 'Excel.Application';
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    ListBox1: TListBox;
+    Button3: TButton;
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  MyExcel:OLEVariant;
+  RibbonControlBar: OleVariant;
+
+implementation
+
+{$R *.dfm}
+function CheckExcelInstall:boolean;
+var
+  ClassID: TCLSID;
+  Rez : HRESULT;
+begin
+// Ищем CLSID OLE-объекта
+  Rez := CLSIDFromProgID(PWideChar(WideString(ExcelApp)), ClassID);
+  if Rez = S_OK then  // Объект найден
+    Result := true
+  else
+    Result := false;
+end;
+
+function CheckExcelRun: boolean;
+begin
+  try
+    MyExcel:=GetActiveOleObject(ExcelApp);
+    Result:=True;
+  except
+    Result:=false;
+  end;
+end;
+
+function RunExcel(DisableAlerts:boolean=true; Visible: boolean=false): boolean;
+begin
+  try
+{проверяем установлен ли Excel}
+    if CheckExcelInstall then
+      begin
+        MyExcel:=CreateOleObject(ExcelApp);
+//показывать/не показывать системные сообщения Excel (лучше не показывать)
+        MyExcel.Application.EnableEvents:=DisableAlerts;
+        MyExcel.Visible:=Visible;
+        Result:=true;
+      end
+    else
+      begin
+        MessageBox(0,'Приложение MS Excel не установлено на этом компьютере','Ошибка',MB_OK+MB_ICONERROR);
+        Result:=false;
+      end;
+  except
+    Result:=false;
+  end;
+end;
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+RunExcel(true, true);
+ListBox1.Items.Clear;
+end;
+
+procedure TForm1.Button2Click(Sender: TObject);
+var CommandBar: variant;
+   i:integer;
+begin
+ for i:=1 to MyExcel.CommandBars.Count do
+   begin
+    CommandBar:=MyExcel.CommandBars.item[i];
+    ListBox1.Items.Add(CommandBar.Name);
+    if CommandBar.Name='Standard' then
+      RibbonControlBar:=CommandBar;
+   end;
+end;
+
+procedure TForm1.Button3Click(Sender: TObject);
+var i:integer;
+    NewControls,Bar:Variant;
+begin
+ListBox1.Items.Clear;
+RibbonControlBar.Reset;
+NewControls:=RibbonControlBar.Controls.Add(Type:=1,id:=1);
+NewControls.Caption:='New Caption';
+NewControls.TooltipText:='c:\Windows\notepad.exe';
+NewControls.HyperlinkType:=1;
+NewControls.FaceId:=10;
+NewControls.Style:=5;
+RibbonControlBar.Parent.LargeButtons:=true;
+if NewControls.BuiltIn then
+  ListBox1.Items.Add('BuiltIn')
+else
+  ListBox1.Items.Add('not BuiltIn')
+end;
+
+end.
